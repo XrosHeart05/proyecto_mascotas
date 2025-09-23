568240408f0f9097ba2716a145fd8a3faaae5183
XrosHeart05 <lucampos2305@gmail.com>
2025-09-22 18:54:36 -0600

    Main Menu

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,6 +7,7 @@ public class Main {
     // Gestor de Reporte de Mascotas
     // Obtener automáticamente la fecha del sistema para cada reporte
 
+    System.out.println(Menu.mainMenu());
     // Posible forma de llamar método para sugerir coincidencias
     // suggests.forEach(p -> System.out.println("Posible coincidencia " + p.getName());
   }
--- src/main/Menu.java
+++ src/main/Menu.java
@@ -2,4 +2,42 @@ package main;
 
 public class Menu {
   // Clase que busca imprimir menus
+
+  public static String centerText(String text, int large) {
+    int spaces = (large - text.length()) / 2;
+    return " ".repeat(spaces) + text;
+  }
+
+  public static String mainMenu() {
+    return mainMenuHeader() + mainMenuBody();
+  }
+  
+  public static String mainMenuHeader() {
+    String text = "";
+    int large = 45;
+    String line1 = "SISTEMA - REFUGIO \"HUELLAS FELICES\"";
+    String line2 = "GESTOR DE REPORTES DE MASCOTAS";
+    text += "=".repeat(large) + "\n";
+    text += centerText(line1, large) + "\n";
+    text += centerText(line2, large) + "\n";
+    text += "=".repeat(large) + "\n";
+
+    return text;
+  }
+  
+  public static String mainMenuBody() {
+    String text = "";
+    int large = 45;
+    text += "1. Registrar mascota desaparecida\n";
+    text += "2. Consultar por ID / Especie / Zona\n";
+    text += "3. Reporte general\n";
+    text += "4. Reporte agrupado\n";
+    text += "5. Ver coincidencias\n";
+    text += "6. Actualizar reporte\n";
+    text += "7. Salir\n";
+    text += "-".repeat(large) + "\n";
+    text += "Ingrese una opción:\n";
+    
+    return text;
+  }
 }
