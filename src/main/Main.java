ca083a199048a89052962f5806fad226cf37e484
XrosHeart05 <lucampos2305@gmail.com>
2025-09-22 19:13:59 -0600

    Select menu options

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,7 +7,11 @@ public class Main {
     // Gestor de Reporte de Mascotas
     // Obtener automáticamente la fecha del sistema para cada reporte
 
-    System.out.println(Menu.mainMenu());
+    int option = 0;
+    do {
+      System.out.println(Menu.mainMenu());
+      option = InputController.inputIntRange(1, 7);
+    } while (option != 7);
     // Posible forma de llamar método para sugerir coincidencias
     // suggests.forEach(p -> System.out.println("Posible coincidencia " + p.getName());
   }
