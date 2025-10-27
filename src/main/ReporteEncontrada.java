d08c7848b8f967ee7f6cafcf66daf29936053378
XrosHeart05 <lucampos2305@gmail.com>
2025-10-27 17:12:31 -0600

    feat (ReporteEncontrada.java): Creación de constructor vacío para uso futuro de sus setters sin asignar valores iniciales

--- src/main/ReporteEncontrada.java
+++ src/main/ReporteEncontrada.java
@@ -3,6 +3,9 @@ package main;
 public class ReporteEncontrada extends Reporte{
   private String tipo;
 
+  public ReporteEncontrada() {
+  }
+  
   public ReporteEncontrada(String id, String reportanteId, String nombreCompleto, String fecha, String zona, String contacto, Mascota mascota) {
     super(id, reportanteId, nombreCompleto, fecha, zona, contacto, mascota);
     this.tipo = "ENC";
